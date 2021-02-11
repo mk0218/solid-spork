a8f510c45166b4dffc7ae1b662bb5b54aac97e69
mkkim <mk0218@postech.ac.kr>
2021-02-12 03:00:40 +0900

    code arrangement

--- hw2/src/App.vue
+++ hw2/src/App.vue
@@ -84,7 +84,7 @@ export default {
 
 <style>
 #app {
-    font-family: 'Noto Sans KR', sans-serif;;
+    font-family: 'Noto Sans KR', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
