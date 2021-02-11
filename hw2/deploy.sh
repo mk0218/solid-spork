404d160baa90e169148aedaeed43a9c5ee4afdb0
mkkim <mk0218@postech.ac.kr>
2021-02-12 04:28:02 +0900

    deploy

--- hw2/deploy.sh
+++ hw2/deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:mk0218/mk0218.github.io.git master
+# git push -f git@github.com:mk0218/mk0218.github.io.git deploy
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:mk0218/solid-spork.git master:gh-pages
+# # if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:mk0218/solid-spork.git deploy:gh-pages
 
 cd -
