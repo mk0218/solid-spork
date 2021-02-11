98839d53484bee4cdfe43b1a16080862946c1206
mkkim <mk0218@postech.ac.kr>
2021-02-12 01:50:33 +0900

    code arrangement

--- hw2/src/App.vue
+++ hw2/src/App.vue
@@ -1,24 +1,26 @@
 <template>
-  <div id="app">
-    <header class="app-header">
-      <app-header>회원가입</app-header>
-    </header>
-    <div class="app-contents">
-        <form-custom class="form" :values="values">
-        <input-plain class="row" v-model="id" placeholder="아이디" type="text"></input-plain>
-        <input-plain class="row" v-model="passwd" placeholder="비밀번호" type="password"></input-plain>
-        <button-xor class="row" value-left="남자" value-right="여자" @change="changeSex"></button-xor>
-        <div style="height: 15px;"></div>
-        <input-label class="row" label="생일">
-          <input-date v-model="birthday"></input-date>
-        </input-label>
-        <input-label class="row" label="전화">
-          <input-phone v-model="phone"></input-phone>
-        </input-label>
-        <agree class="agree" @toggle="toggleAgree"></agree>
-      </form-custom>
-      </div>
-  </div>
+    <div id="app">
+        <header class="app-header">
+            <app-header>회원가입</app-header>
+        </header>
+        <div class="app-contents">
+            <form-custom class="form" :values="values">
+                <input-plain class="row" v-model="id" placeholder="아이디" type="text"></input-plain>
+                <input-plain class="row" v-model="passwd" placeholder="비밀번호" type="password"></input-plain>
+                <button-xor class="row" value-left="남자" value-right="여자" @change="changeSex"></button-xor>
+                <!-- Empty space -->
+                <div style="height: 15px;"></div>
+
+                <input-label class="row" label="생일">
+                    <input-date v-model="birthday"></input-date>
+                </input-label>
+                <input-label class="row" label="전화">
+                    <input-phone v-model="phone"></input-phone>
+                </input-label>
+                <agree class="agree" @toggle="toggleAgree"></agree>
+            </form-custom>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -33,79 +35,79 @@ import FormCustom from "./components/FormCustom.vue"
 
 let today = new Date();
 let todayStr = today.getFullYear().toString() + "/" +
-    (today.getMonth() + 1).toString() + "/" + today.getDate().toString();
+               (today.getMonth() + 1).toString() + "/" + today.getDate().toString();
 
 export default {
-  name: 'App',
-  components: {
-    'app-header': AppHeader,
-    'button-xor': ButtonXor,
-    'agree': Agree,
-    'form-custom': FormCustom,
-    'input-date': InputDate,
-    'input-label': InputLabeled,
-    'input-phone': InputPhone,
-    'input-plain': InputPlain,
-  },
-  data: function () {
-    return {
-      id: '',
-      passwd: '',
-      sex: '남자',
-      birthday: todayStr,
-      phone: '',
-      agree: false,
-    };
-  },
-  computed: {
-    values: function () {
-      return {
-        id: this.id,
-        password: this.passwd,
-        sex: this.sex,
-        birthday: this.birthday,
-        phone: this.phone,
-        agree: this.agree
-      };
-    }
-  },
-  methods: {
-    changeSex: function (sex) {
-      this.sex = sex;
+    name: 'App',
+    components: {
+        'agree': Agree,
+        'app-header': AppHeader,
+        'button-xor': ButtonXor,
+        'form-custom': FormCustom,
+        'input-date': InputDate,
+        'input-label': InputLabeled,
+        'input-phone': InputPhone,
+        'input-plain': InputPlain,
     },
-    toggleAgree: function () {
-      this.agree = !this.agree;
+    data: function () {
+        return {
+            id: '',
+            passwd: '',
+            sex: '남자',
+            birthday: todayStr,
+            phone: '',
+            agree: false,
+        };
+    },
+    computed: {
+        values: function () {
+            return {
+                id: this.id,
+                password: this.passwd,
+                sex: this.sex,
+                birthday: this.birthday,
+                phone: this.phone,
+                agree: this.agree
+            };
+        }
+    },
+    methods: {
+        changeSex: function (sex) {
+            this.sex = sex;
+        },
+        toggleAgree: function () {
+            this.agree = !this.agree;
+        }
     }
-  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Noto Sans KR', sans-serif;;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  overflow: hidden;
-  height: 90vh;
-  padding: 8vw;
+    font-family: 'Noto Sans KR', sans-serif;;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    overflow: hidden;
+    height: 90vh;
+    padding: 8vw;
 }
 .app-header {
-  position: relative;
-  height: 13%;
+    position: relative;
+    height: 13%;
 }
 .app-contents {
-  position: relative;
-  height: 87%;
+    position: relative;
+    height: 87%;
 }
 .row {
-  width: 100%;
-  height: 40px;
-  margin-bottom: 14px;
+    width: 100%;
+    height: 40px;
+    margin-bottom: 14px;
 }
 .agree {
-  position: absolute;
-  bottom: 80px;
+    position: absolute;
+    bottom: 80px;
 }
 
 </style>
--- hw2/src/components/Agree.vue
+++ hw2/src/components/Agree.vue
@@ -16,8 +16,7 @@ export default {
 
 <style scoped>
 .agree {
-  /*display: flex;*/
-  width: 100%;
+    width: 100%;
 }
 span {
     font-size: 1rem;
@@ -25,65 +24,63 @@ span {
 .switch-box {
     position: absolute;
 }
+
 /* The switch - the box around the slider */
 .switch {
-  position: absolute;
-  top: 6px;
-  right: 0;
-  width: 40px;
-  height: 20px;
+    position: absolute;
+    top: 6px;
+    right: 0;
+    width: 40px;
+    height: 20px;
 }
+
 /* Hide default HTML checkbox */
 .switch input {
-  opacity: 0;
-  width: 0;
-  height: 0;
+    opacity: 0;
+    width: 0;
+    height: 0;
 }
+
 /* The slider */
 .slider {
-  position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #ccc;
-  -webkit-transition: .4s;
-  transition: .4s;
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ccc;
+    -webkit-transition: .4s;
+    transition: .4s;
 }
-
 .slider:before {
-  position: absolute;
-  content: "";
-  height: 14px;
-  width: 14px;
-  left: 3px;
-  bottom: 3px;
-  background-color: white;
-  -webkit-transition: .4s;
-  transition: .4s;
+    position: absolute;
+    content: "";
+    height: 14px;
+    width: 14px;
+    left: 3px;
+    bottom: 3px;
+    background-color: white;
+    -webkit-transition: .4s;
+    transition: .4s;
 }
-
 input:checked + .slider {
-  background-color: #2196F3;
+    background-color: #2196F3;
 }
-
 input:focus + .slider {
-  box-shadow: 0 0 1px #2196F3;
+    box-shadow: 0 0 1px #2196F3;
 }
-
 input:checked + .slider:before {
-  -webkit-transform: translateX(20px);
-  -ms-transform: translateX(20px);
-  transform: translateX(20px);
+    -webkit-transform: translateX(20px);
+    -ms-transform: translateX(20px);
+    transform: translateX(20px);
 }
 
 /* Rounded sliders */
 .slider.round {
-  border-radius: 10px;
+    border-radius: 10px;
 }
-
 .slider.round:before {
-  border-radius: 50%;
+    border-radius: 50%;
 }
 </style>
